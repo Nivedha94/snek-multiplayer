23b56e866eab1255b73af5d3a8180c4e511dc6aa
vagrant <nivedha.selvan@gmail.com>
2021-11-04 19:44:46 +0000

    event handler

--- client.js
+++ client.js
@@ -6,6 +6,11 @@ const connect = function () {
     port: 50541
   });
 
+  conn.on("connect",function() {
+    console.log("Successfully connected to game server");
+    conn.write('Name: Niv');
+  })
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
