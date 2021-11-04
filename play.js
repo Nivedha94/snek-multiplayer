3d2e463c76357157c95ed7c71c63503e1cbeb133
vagrant <nivedha.selvan@gmail.com>
2021-11-04 20:10:22 +0000

    refactoring

--- play.js
+++ play.js
@@ -13,23 +13,10 @@ game.start()
 
 
 const connect = require("./client.js");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 setupInput();
+
